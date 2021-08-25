a4bf592c7a8962d45875890553868746199b0bcc
Youssouf Kacemi <youssouf.kacemi@gmail.com>
2021-08-25 11:46:01 +0100

    adding suggestions part, with just skelton.

--- src/components/sidebar/suggestions.jsx
+++ src/components/sidebar/suggestions.jsx
@@ -1,5 +1,16 @@
+import Skeleton from "react-loading-skeleton";
+import { Link } from "react-router-dom";
+import * as ROUTES from '../../constants/routes';
 export default function Suggestions() {
     return (
-        <h1>suggestions</h1>
+        <div>
+            <div className='flex items-center justify-between mb-3'>
+                <h1 className="font-bold text-gray-400">Suggestions For You</h1>
+                <Link to={ROUTES.SUGGESTIONS} >
+                    <p className="text-xs font-bold text-gray-900">See All</p>
+                </Link>
+            </div>
+            <Skeleton count={5} height={45} />
+        </div>
     )
 }
